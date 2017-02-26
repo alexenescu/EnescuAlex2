b6ae35f5e379106b0e154433b21c826be71cbcc2
admin <admin@w701>
2017-02-26 18:03:25 +0100

    V 2.0

--- Fecha2/src/Fecha/Fecha.java
+++ Fecha2/src/Fecha/Fecha.java
@@ -1,8 +1,11 @@
 package Fecha;
+
 public class Fecha {
+	
 	private int dia;
 	private int mes;
 	private int anio;
+	
 	public Fecha(int dia, int mes, int anio) {
 		this.dia = dia;
 		this.mes = mes;
